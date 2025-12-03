636df698d373a0b688ef03698fa5f9072390f1e3
zZEnergiZz <74988478@continental.edu.pe>
2025-12-03 10:53:53 -0500

    Actualizar variables de entorno para producción

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
-// db.js
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'web_artesano',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
